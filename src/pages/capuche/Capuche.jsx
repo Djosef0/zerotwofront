740aea3d406b01a8fcd4f5d55d422f45386c12f2
jayakrishnamurthy <youssefbensaid827@gmail.com>
2022-10-05 01:35:48 +0200

    second Commit

--- src/pages/capuche/Capuche.jsx
+++ src/pages/capuche/Capuche.jsx
@@ -13,7 +13,7 @@ useEffect(() => {
  
     const getCapuche = async () => {
       try {
-        const res = await axiosInstance.get(Url+"/clothers/capuche", {
+        const res = await axios.get(Url+"/clothers/capuche", {
           headers: {
             token:
             "Bearer "+JSON.parse(localStorage.getItem("user")).accessToken,
